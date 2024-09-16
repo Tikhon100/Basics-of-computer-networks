deba0ef7beee9ca2ad6e237f65a9cb0b93a32304
tikhon <25050056@study.bsuir.by>
2024-09-16 22:49:32 +0300

    last version))

--- widget.py
+++ widget.py
@@ -2,13 +2,16 @@ import sys
 import serial
 import glob
 import re
-
+from packet import Packet
 from PySide6.QtWidgets import QApplication, QWidget
 from PySide6.QtCore import Slot
 from ui_form import Ui_Widget
 from text_edit_logger import QTextEditLogger
+import packet
+
 
 import logging
+from typing import List
 # this is sparta
 class Widget(QWidget):
     def __init__(self, parent=None):
@@ -26,6 +29,7 @@ class Widget(QWidget):
         self.add_port_pairs_to_combobox(available_port_pairs)
         available_speed = [50, 75, 110, 134, 150, 200, 300, 600, 1200, 1800, 2400, 4800, 9600, 19200, 38400, 57600, 115200]
         self.ui.comboBox_2.addItems(map(str, available_speed))
+
 
     def get_available_port_pairs(self):
         virtual_ports = []
@@ -51,6 +55,18 @@ class Widget(QWidget):
                 self.ui.comboBox.addItem(f"{com_port_name1} - {com_port_name2}")
                 com_port_counter += 2
 
+    def create_packets(self, input_string: str, flag: bytes, destination_address: bytes, source_address: bytes) -> List[Packet]:
+        packets = []
+        input_bytes = input_string.encode()
+
+        for i in range(0, len(input_bytes), 15):
+            chunk = input_bytes[i:i + 15]
+            packet = Packet(flag, destination_address, source_address, chunk)
+            packets.append(packet)
+
+        return packets
+
+
     def send_data(self):
         try:
             selected_port_pair = self.ui.comboBox.currentText()
@@ -61,28 +77,43 @@ class Widget(QWidget):
 
             speed = self.ui.comboBox_2.currentText()
 
-            logging.info(f"Initialization ports {com_port1}, {com_port2} with baud {speed}...")
+
             ser1 = serial.Serial(port1, baudrate=speed, timeout=1)
             ser2 = serial.Serial(port2, baudrate=speed, timeout=1)
-            logging.info("Ports initialization was successfull!")
 
-            data_to_send = self.ui.inputTextEdit.toPlainText().encode()
-            sent_data_length = len(data_to_send)
+
+            data_to_send = self.ui.inputTextEdit.toPlainText()
+            packets = self.create_packets(data_to_send, "00001110", "0000", port1)
+            #sent_data_length = len(data_to_send)
             self.ui.inputTextEdit.clear()
-            logging.info(f"Sending data through a port {com_port1}, data len is {sent_data_length} bytes...")
-            bytes_written = ser1.write(data_to_send)
-            logging.info(f"Successfully sended {bytes_written} bytes!")
+            for packet in packets:
+                str1 = ' '.join(format(b, '08b') for b in packet.data_before_stuffing)
+                str2 = ' '.join(format(b, '08b') for b in packet.data_after_stuffing)
+                str3 = ','.join(str(b) for b in packet.data_after_stuffing)
+                char_list = [chr(int(b)) for b in str3.split(',')]
+                result_string = ''.join(char_list)
 
-            logging.info(f"Reading data through a port {com_port2}...");
-            data_received = ser2.read(len(data_to_send))
-            self.ui.outputTextEdit.setPlainText(data_received.decode())
-            received_data_length = len(data_received)
-            logging.info(f"Successfully readed {received_data_length} bytes!");
 
-            logging.info(f"Closing ports {com_port1}, {com_port2}...")
-            ser1.close()
-            ser2.close()
-            logging.info("Ports successfully closed!")
+                self.ui.statusTextEdit.append(f"Data before stuffing (binary): {str1}")
+                self.ui.statusTextEdit.append(f"Data  after stuffing (binary): {str2}")
+
+                data_to_send = f"{packet.flag}{packet.destination_address}{packet.source_address}~{result_string}~{packet.fcs}".encode()
+
+                ser1.write(data_to_send)
+
+            #bytes_written = ser1.write(data_to_send)
+
+
+
+            #data_received = ser2.read(len(data_to_send))
+            #self.ui.outputTextEdit.setPlainText(data_received.decode())
+            #received_data_length = len(data_received)
+
+
+
+            #ser1.close()
+            #ser2.close()
+
         except serial.SerialException as e:
             logging.error(f"Ошибка при работе с последовательным портом: {e}")
         except Exception as e:
@@ -100,6 +131,8 @@ class Widget(QWidget):
     @Slot()
     def send_button_click(self):
         self.send_data()
+
+
 
 
 if __name__ == "__main__":
